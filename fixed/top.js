da8b691da3fa7f20b0e92d205d20986c55214c1c
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-08-15 16:54:16 +0900

    [UPDATE] 포레스티아 로고 누르면 Home 페이지로 이동

--- fixed/top.js
+++ fixed/top.js
@@ -1,11 +1,15 @@
 import { CgTrees } from 'react-icons/cg';
 import { HiOutlineKey } from 'react-icons/hi';
+import { useDispatch } from '../pages/_context';
 import St_top from '../styles/fixed/St_top';
+import { unmountAnimation } from './AnimationController';
 
 export default function Top() {
+  const dispatch = useDispatch();
+
   return (
     <St_top>
-      <div className="forestia_logo">
+      <div className="forestia_logo" onClick={() => unmountAnimation(0, dispatch, "/home")}>
         <CgTrees />
         &nbsp;Forestia.
       </div>
