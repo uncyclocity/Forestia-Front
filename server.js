c9e5ca4fe9505a2f8031cd88f4c7fbfaea02c914
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-08-19 14:24:13 +0900

    [UPDATE] 도큐먼트 추가 코드 주석처리

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const next = require('next');
 require('dotenv').config();
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const app_e = express();
 const handle = app.getRequestHandler();
 const Post = require('./models/post');
 const { CONNECT_URI } = process.env;
@@ -54,48 +53,39 @@ app
     process.exit(1);
   });
 
-app_e.use((req, res, next) => {
-  return Post.find()
-    .then((postsArray) => {
-      console.log(postsArray);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// const post1 = new Post({
+//   id: 1,
+//   author: '백괴',
+//   date: '2021-08-02 22:39:08',
+//   title: '테스트2',
+//   content: '자게/테스트2 입니다.',
+//   comments: [
+//     {
+//       id: 0,
+//       author: '백괴',
+//       date: '2021-08-02 22:39:08',
+//       content: '자게/테스트2/댓글1 입니다.',
+//     },
+//     {
+//       id: 1,
+//       author: '백괴',
+//       date: '2021-08-02 22:39:08',
+//       content: '자게/테스트2/댓글2 입니다.',
+//     },
+//     {
+//       id: 2,
+//       author: '백괴',
+//       date: '2021-08-02 22:39:08',
+//       content: '자게/테스트2/댓글3 입니다.',
+//     },
+//   ],
+// });
 
-const post1 = new Post({
-  id: 1,
-  author: '백괴',
-  date: '2021-08-02 22:39:08',
-  title: '테스트2',
-  content: '자게/테스트2 입니다.',
-  comments: [
-    {
-      id: 0,
-      author: '백괴',
-      date: '2021-08-02 22:39:08',
-      content: '자게/테스트2/댓글1 입니다.',
-    },
-    {
-      id: 1,
-      author: '백괴',
-      date: '2021-08-02 22:39:08',
-      content: '자게/테스트2/댓글2 입니다.',
-    },
-    {
-      id: 2,
-      author: '백괴',
-      date: '2021-08-02 22:39:08',
-      content: '자게/테스트2/댓글3 입니다.',
-    },
-  ],
-});
-
-post1
-  .save()
-  .then(() => {
-    console.log('성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// post1
+//   .save()
+//   .then(() => {
+//     console.log('성공');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
