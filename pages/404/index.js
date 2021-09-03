5cce78bf2df62f0f462fce87abae9334de3325bb
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-09-03 17:40:55 +0900

    [U. D.] 404 페이지 및 about 페이지 스타일-JS 일체화 및 불필요 json 제거

--- pages/404/index.js
+++ pages/404/index.js
@@ -5,7 +5,98 @@ import {
   unmountAnimation,
   mountAnimation,
 } from '../../src/animationController';
-import St_404 from '../../styles/pages/St_404';
+import FourAnimationedBox from '../../src/boxEle/FourAnimationdBox';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100px;
+  padding: 20px;
+  color: #828c99;
+
+  .errorcode {
+    height: 100%;
+    padding: 20px 30px 20px 20px;
+    font-size: 21px;
+
+    color: #20c997;
+    border-right: 1px solid #e9ecef;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .error_icon {
+      font-size: 26px;
+      transform: translateY(10%);
+    }
+  }
+
+  .btns {
+    padding: 20px 10px 20px 20px;
+
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+
+    .back_btn {
+      width: 60px;
+
+      margin: 10px 10px;
+      padding: 8px;
+
+      text-align: center;
+      font-size: 15px;
+
+      box-shadow: 0px 0px 15px #9aefd6;
+
+      color: white;
+      background: #20c997;
+
+      border-radius: 15px;
+
+      &:hover {
+        transition: 0.25s all ease-in;
+        box-shadow: 0px 0px 15px #36deac;
+        cursor: pointer;
+      }
+
+      &:not(:hover) {
+        transition: 0.25s all ease-in;
+      }
+    }
+
+    .home_btn {
+      width: 60px;
+
+      margin: 10px 10px;
+      padding: 8px;
+
+      text-align: center;
+      font-size: 15px;
+
+      box-shadow: 0px 0px 15px #9aefd6;
+
+      color: #20c997;
+      background: white;
+
+      border-radius: 15px;
+
+      &:hover {
+        transition: 0.25s all ease-in;
+        box-shadow: 0px 0px 15px #36deac;
+        cursor: pointer;
+      }
+
+      &:not(:hover) {
+        transition: 0.25s all ease-in;
+      }
+    }
+  }
+`;
 
 export default function NotFoundPage() {
   const dispatch = useDispatch();
@@ -15,24 +106,29 @@ export default function NotFoundPage() {
   }, [dispatch]);
 
   return (
-    <St_404>
-      <div className="errorcode">
-        <div className="error_icon">
-          <VscError />
+    <FourAnimationedBox>
+      <Styles>
+        <div className="errorcode">
+          <div className="error_icon">
+            <VscError />
+          </div>
+          &nbsp;404 Error
         </div>
-        &nbsp;404 Error
-      </div>
-      <div className="btns">
-        <div className="back_btn" onClick={() => unmountAnimation(1, dispatch)}>
-          뒤로
+        <div className="btns">
+          <div
+            className="back_btn"
+            onClick={() => unmountAnimation(1, dispatch)}
+          >
+            뒤로
+          </div>
+          <div
+            className="home_btn"
+            onClick={() => unmountAnimation(0, dispatch, '/home')}
+          >
+            메인
+          </div>
         </div>
-        <div
-          className="home_btn"
-          onClick={() => unmountAnimation(0, dispatch, '/home')}
-        >
-          메인
-        </div>
-      </div>
-    </St_404>
+      </Styles>
+    </FourAnimationedBox>
   );
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -3,8 +3,45 @@ import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { SiNextDotJs } from 'react-icons/si';
 import { useEffect } from 'react';
 import { mountAnimation } from '../../src/animationController';
-import St_about from '../../styles/pages/St_about';
 import { useDispatch } from '../../src/context';
+import FourAnimationedBox from '../../src/boxEle/FourAnimationdBox';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  padding: 20px 30px 5px 30px;
+
+  .content {
+    color: #5d5d5d;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+
+    .next_logo {
+      color: #20c997;
+      height: 100px;
+      margin: 30px 40px 20px 40px;
+      font-size: 90px;
+    }
+
+    .text {
+      border-left: 1px solid #e9ecef;
+      padding-left: 40px;
+      .line {
+        display: flex;
+        flex-direction: row;
+        .bold {
+          color: #20c997;
+          font-weight: bold;
+        }
+        &:not(:last-child) {
+          margin-bottom: 3px;
+        }
+      }
+    }
+  }
+`;
 
 export default function About() {
   const dispatch = useDispatch();
@@ -14,30 +51,32 @@ export default function About() {
   }, [dispatch]);
 
   return (
-    <St_about>
-      <Board_title backURL="/home">
-        <div className="icon">
-          <AiOutlineInfoCircle />
-        </div>
-        게시판 소개
-      </Board_title>
-      <div className="content">
-        <div className="next_logo">
-          <SiNextDotJs />
-        </div>
-        <div className="text">
-          <div className="line">본 게시판은</div>
-          <div className="line">
-            <div className="bold">백괴</div>가 Next.js의&nbsp;
-            <div className="bold">라우팅</div>&nbsp;및&nbsp;
-            <div className="bold">링크 컴포넌트</div>를
+    <FourAnimationedBox>
+      <Styles>
+        <Board_title backURL="/home">
+          <div className="icon">
+            <AiOutlineInfoCircle />
           </div>
-          <div className="line">연습할 목적으로 만들어졌습니다.</div>
-          <div className="line">
-            장래가 기대되는 백괴에게 뜨거운 박수를 부탁드립니다.
+          게시판 소개
+        </Board_title>
+        <div className="content">
+          <div className="next_logo">
+            <SiNextDotJs />
+          </div>
+          <div className="text">
+            <div className="line">본 게시판은</div>
+            <div className="line">
+              <div className="bold">백괴</div>가 Next.js의&nbsp;
+              <div className="bold">라우팅</div>&nbsp;및&nbsp;
+              <div className="bold">링크 컴포넌트</div>를
+            </div>
+            <div className="line">연습할 목적으로 만들어졌습니다.</div>
+            <div className="line">
+              장래가 기대되는 백괴에게 뜨거운 박수를 부탁드립니다.
+            </div>
           </div>
         </div>
-      </div>
-    </St_about>
+      </Styles>
+    </FourAnimationedBox>
   );
 }
