f59ebe4b778b7d6681b3e8b8114c24a79f0c942d
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-09-05 13:59:17 +0900

    [C. U. D.] src/posting 디렉터리명 변경
    posting ➡️ board.posting

--- pages/board/posting/index.js
+++ pages/board/posting/index.js
@@ -5,15 +5,15 @@ import { useEffect } from 'react';
 import {
   postPageSwitchOff,
   postPageSwitchOn,
-} from '../../../src/posting/etcFunc/postpageSwitching';
-import getPostingEleState from '../../../src/posting/etcFunc/getPostingEleState';
-import UpAndDown from '../../../src/posting/pageEle/upAndDown';
-import InBoardTitle from '../../../src/posting/pageEle/inBoardTitle';
-import CommentInput from '../../../src/posting/pageEle/commentInput';
-import CommentList from '../../../src/posting/pageEle/commentList';
+} from '../../../src/board.posting/etcFunc/postpageSwitching';
+import getPostingEleState from '../../../src/board.posting/etcFunc/getPostingEleState';
+import UpAndDown from '../../../src/board.posting/pageEle/upAndDown';
+import InBoardTitle from '../../../src/board.posting/pageEle/inBoardTitle';
+import CommentInput from '../../../src/board.posting/pageEle/commentInput';
+import CommentList from '../../../src/board.posting/pageEle/commentList';
 import styled from 'styled-components';
 import FourAnimationedBox from '../../../src/boxEle/FourAnimationdBox';
-import ContentView from '../../../src/posting/pageEle/contentView';
+import ContentView from '../../../src/board.posting/pageEle/contentView';
 import { mountAnimation } from '../../../src/common/animationController';
 
 const BoxStyles = styled.div`
