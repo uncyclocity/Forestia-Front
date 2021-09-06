d5d797b695447ed4ad97a4510b5efac272cdd253
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-09-06 10:20:46 +0900

    [UPDATE] 최상단 로고 단순화

--- src/header/headerEle/top.js
+++ src/header/headerEle/top.js
@@ -1,4 +1,3 @@
-import { AiOutlineUser } from 'react-icons/ai';
 import { CgTrees } from 'react-icons/cg';
 import { useDispatch } from '../../common/context';
 import styled from 'styled-components';
@@ -11,41 +10,16 @@ const Styles = styled.div`
   justify-content: center;
   width: 700px;
   margin-top: 30px;
-  flex-direction: row;
 
   .forestia_logo {
     display: flex;
-    margin-right: auto;
-    flex-direction: row;
-    color: #20c997;
-    font-family: Helvetica;
-    font-size: 50px;
-    font-weight: bold;
-    cursor: pointer;
-  }
-
-  .profile .signin_btn {
-    background: #20c997;
-    color: white;
-    width: 50px;
-    height: 50px;
-    border-radius: 50px;
-    display: flex;
     align-items: center;
     justify-content: center;
-    font-weight: bold;
-    font-size: 30px;
-    box-shadow: 0px 6px 10px #9aefd6;
-
-    &:hover {
-      transition: 0.25s all ease-in;
-      box-shadow: 0px 6px 10px #36deac;
-      cursor: pointer;
-    }
-
-    &:not(:hover) {
-      transition: 0.25s all ease-in;
-    }
+    flex-direction: column;
+    color: #20c997;
+    cursor: pointer;
+    height: 50px;
+    font-size: 50px;
   }
 `;
 
@@ -59,12 +33,6 @@ export default function Top() {
         onClick={() => unmountAnimation(0, dispatch, '/home')}
       >
         <CgTrees />
-        &nbsp;Forestia.
-      </div>
-      <div className="profile">
-        <div className="signin_btn">
-          <AiOutlineUser />
-        </div>
       </div>
     </Styles>
   );
