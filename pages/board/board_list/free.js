9efa2b7707097a613690e34efb468a87e9416030
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-09-12 12:14:02 +0900

    [UPDATE] 배열 리버스 기준점 수시로 바뀌는 버그 수정(2트)

--- pages/board/board_list/free.js
+++ pages/board/board_list/free.js
@@ -96,7 +96,7 @@ export default function Free() {
           <BoardTitle backURL="/home">
             <InFreeListBoardTitle />
           </BoardTitle>
-          <FreeListPostingList page={page} freeBoard={freeBoard.reverse()} />
+          <FreeListPostingList page={page} freeBoard={freeBoard} />
           <PageBtn freeBoard={freeBoard} page={page} />
         </ListStyle>
       </BoxStyles>
