2e613821de8014585d4b65bf26f7f0e2134285dc
play.js <git@gituser.com>
2021-10-08 19:48:55 +0900

    [UPDATE] 로그인X시 좋아요 싫어요 버튼 안보이도록 설저ㅇ

--- components/Organisms/PostingContentAndUpDown.js
+++ components/Organisms/PostingContentAndUpDown.js
@@ -1,19 +1,21 @@
 import PostingContentView from '../MoleCules/PostingContentView';
 import PostingImageView from '../MoleCules/PostingImageView';
 import PostingUpAndDown from '../MoleCules/PostingUpAndDown';
+import { useReducerState } from '../../src/context';
 
 export default function PostingContentAndUpDown({
   nowPostingEleObj,
   setNowPostingEleObj,
 }) {
+  const userId = useReducerState().user.userId;
   return (
     <>
       <PostingContentView nowPostingEleObj={nowPostingEleObj} />
       <PostingImageView nowPostingEleObj={nowPostingEleObj} />
-      <PostingUpAndDown
+      {userId && <PostingUpAndDown
         nowPostingEleObj={nowPostingEleObj}
         setNowPostingEleObj={setNowPostingEleObj}
-      />
+      />}
     </>
   );
 }
