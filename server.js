4ee7c23148991d43c7685e0a3b68592f553d6b25
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-10-13 13:58:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, (err) => {
+    server.listen($PORT, (err) => {
       if (err) throw err;
-      console.log('3000번 포트에서 대기 중');
+      console.log('$PORT번 포트에서 대기 중');
     });
   })
   .catch((ex) => {
