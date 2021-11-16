0339ff87db6c5d0d147288b6e4162a1c60da45b2
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-11-16 17:57:46 +0900

    Update commDeleting.js

--- pages/board/update_comment/commDeleting.js
+++ pages/board/update_comment/commDeleting.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Router, useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { useDispatch } from '../../../src/context';
 import DeletingTemplate from '../../../components/Templates/DeletingTemplate';
 import { postComm } from '../../../src/doApi';
@@ -9,7 +9,7 @@ const letsDeleteComm = async (
   dispatch,
   { board_type, post_id, comment_id },
 ) => {
-  if (board_type && post_id && comment_id) {
+  if (board_type || post_id || comment_id) {
     dispatch({
       type: 'postcnt_switcher',
       sw: true,
@@ -27,12 +27,13 @@ const letsDeleteComm = async (
 export default function CommDeleting() {
   const router = useRouter();
   const dispatch = useDispatch();
-
+  console.log(router);
   useEffect(() => {
     dispatch({
       type: 'initiate',
       nowPage: 'commDeleting',
     });
+
     letsDeleteComm(dispatch, router.query);
   }, [dispatch, router.query]);
 
