1bf388abe16baced327d6b4aa47cfecd29a6ea29
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-12-07 21:04:05 +0900

    [UPDATE] 개인 도메인(uncyclocity.tech)로 설정

--- src/instance.js
+++ src/instance.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: 'https://forestia.herokuapp.com',
+  baseURL: 'http://uncyclocity.tech:3050',
   headers: {
     'Content-Type': 'application/json',
   },
