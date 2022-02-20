f723bd288629d932e02cc55ff528e512fd6109f4
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2022-02-20 15:52:40 +0900

    [C. U.] 프로필 사진 변경 버튼 추가

---
+++ components/Atoms/Button/BtnProfilePhotoChange.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+// BtnProfilePhotoChange
+// 분류 : 버튼
+// 용도 : 프로필 사진 변경 버튼
+
+const Styles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-contents: center;
+  position: absolute;
+  border-radius: 50px;
+  cursor: pointer;
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  background: black;
+  opacity: 0.5;
+  color: white;
+  font-size: ${({ size }) => size}px;
+`;
+
+export default function BtnProfilePhotoChange({
+  onClick,
+  text,
+  size,
+  width,
+  height,
+}) {
+  return (
+    <Styles onClick={onClick} width={width} height={height} size={size}>
+      {text}
+    </Styles>
+  );
+}
--- components/MoleCules/ModalAccountSettingsProfile.js
+++ components/MoleCules/ModalAccountSettingsProfile.js
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { BiUser } from 'react-icons/bi';
 import styled from 'styled-components';
 import HicProfilePhoto from '../Atoms/HybridIcon/HicProfilePhoto';
+import BtnProfilePhotoChange from '../Atoms/Button/BtnProfilePhotoChange';
 import TxtProfile from '../Atoms/Text/TxtProfile';
 
 const Styles = styled.div`
@@ -17,17 +19,33 @@ const Styles = styled.div`
 `;
 
 export default function ModalAccountSettingsProfile({ user }) {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <Styles>
-      <HicProfilePhoto
-        statusIcon={<BiUser />}
-        bgColor="#20c997"
-        color="white"
-        shadowColor="#dedede"
-        size="70"
-        padding="15"
-        imageUrl={user.imageUrl}
-      />
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        {isHover && (
+          <BtnProfilePhotoChange
+            text="프로필 사진 변경"
+            size="14"
+            width="100"
+            height="100"
+            onClick={() => alert('준비중입니다.')}
+          />
+        )}
+        <HicProfilePhoto
+          statusIcon={<BiUser />}
+          bgColor="#20c997"
+          color="white"
+          shadowColor="#dedede"
+          size="70"
+          padding="15"
+          imageUrl={user.imageUrl}
+        />
+      </div>
       <TxtProfile userName={user.userName} color="#20c997" size="21" />
       <TxtProfile userName={user.userEmail} color="#828c99" size="15" />
     </Styles>
